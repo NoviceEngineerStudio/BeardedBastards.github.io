20ec646c75bbc4022d291df4597808326e5b66a5
Novice Engineer <skyler32702@gmail.com>
2025-12-03 17:34:10 -0600

    Service Cards and List

--- src/content/services.ts
+++ src/content/services.ts
@@ -1,5 +1,7 @@
 import type { TranslationFileKey } from "../utils/content/translation";
 
+import ASHLEY from "../assets/images/ashley_deployment.webp";
+
 interface Service {
     preview_image: ImageMetadata;
     name_translation_key: TranslationFileKey["services"];
@@ -7,7 +9,35 @@ interface Service {
 };
 
 const services: Service[] = [
-
+    {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }, {
+        preview_image: ASHLEY,
+        name_translation_key: "example_title",
+        description_translation_key: "example_description"
+    }
 ];
 
 export { type Service };
